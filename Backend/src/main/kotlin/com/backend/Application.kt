339ce6cac45bec3dbe7ea128c4f26ea3fe55fa26
priyamprakash <priyamprakash99@gmail.com>
2023-12-25 07:26:54 +0530

    default header issue resolved

--- Backend/src/main/kotlin/com/backend/Application.kt
+++ Backend/src/main/kotlin/com/backend/Application.kt
@@ -4,6 +4,7 @@ import com.backend.plugins.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.defaultheaders.*
 
 fun main() {
     embeddedServer(Netty, port = 8001, host = "0.0.0.0", module = Application::module)
@@ -12,4 +13,5 @@ fun main() {
 
 fun Application.module() {
     configureRouting()
+    install(DefaultHeaders)
 }
