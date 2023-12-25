d5bcef9663c212468bb036c0bedb2e0af487eba8
priyamprakash <priyamprakash99@gmail.com>
2023-12-25 08:05:55 +0530

    Call logging , content negotiation

--- Backend/src/main/kotlin/com/backend/Application.kt
+++ Backend/src/main/kotlin/com/backend/Application.kt
@@ -5,6 +5,11 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.defaultheaders.*
+import io.ktor.server.plugins.callloging.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+
 
 fun main() {
     embeddedServer(Netty, port = 8001, host = "0.0.0.0", module = Application::module)
@@ -14,4 +19,13 @@ fun main() {
 fun Application.module() {
     configureRouting()
     install(DefaultHeaders)
+    install(CallLogging)
+//    install(Routing)
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+        })
+    }
+
+
 }
